7a0ef49399838972bc7d4d1887dc2444be4884fc
Nuabtec <nubatec@gmail.com>
2017-10-04 18:34:52 -0500

    avance
    avance 2, de ejercicios

---
+++ ejercicio2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio-2</title>
+</head>
+<body>
+
+	<p><b>  </b></p>
+
+</body>
+</html>
---
+++ ejerciciox.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio-x</title>
+</head>
+<body>
+
+	<p><b>  </b></p>
+
+</body>
+</html>
