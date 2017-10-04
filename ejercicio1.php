75249032d86e7123343a66f01e73ada1b2303db7
Nuabtec <nubatec@gmail.com>
2017-10-04 18:25:13 -0500

    Prueba 1
    Prueba de práctica php y github

---
+++ ejercicio1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicios-1</title>
+</head>
+<body>
+
+<p><b>1 - Concatenar dos cadenas e imprimir su resultado</b></p>
+
+<?php
+
+$cadena1 = "¡Código ";
+$cadena2 = "PHP!";
+
+$cadenas = $cadena1 . $cadena2;
+
+
+echo $cadenas;
+
+?>
+
+</body>
+</html>
