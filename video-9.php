4cd6b5972d9ec9ad3f51da60bd28a3cbbfe1461f
Nuabtec <nubatec@gmail.com>
2017-10-09 18:03:53 -0500

    fin de semana

---
+++ video-9.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Video-9</title>
+</head>
+<body>
+
+	<p><b> Operadores de comparación </b></p>
+	<a href="https://www.youtube.com/watch?v=gYbhDXhJz6E&t=193s"> Enlace del video </a>
+	<br>
+	<br>
+	<br>
+	<br>
+
+	<?php
+
+	$variable1 = 8;
+
+	$variable2 = "8";
+
+	$variable3 = "Juan";
+
+	if ($variable1 == $variable3) {
+
+		echo "Son iguales";
+
+	} else {
+
+		echo "No son iguales";
+
+	}
+
+	?>
+
+</body>
+</html>
---
+++ video-x.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Video-x</title>
+</head>
+<body>
+
+	<p><b>  </b></p>
+
+
+</body>
+</html>
